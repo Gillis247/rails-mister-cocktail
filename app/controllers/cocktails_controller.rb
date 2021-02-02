4550fb5d250166f8f4aea8798d23c0e7b47b4d3e
Gillis Rikardsson <gillis.rikardsson@gmail.com>
2021-02-02 20:01:41 +0000

    done with basic setup

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,21 +1,30 @@
 class CocktailsController < ApplicationController
 
   def index
-    @cocktails = Cocktail.all
+   @cocktails = Cocktail.all
   end
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def new
     @cocktail = Cocktail.new
   end
 
-#private
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
 
-  #def cocktail_params
-    #params.require(:cocktail).permit(:name, :doses, :ingredient)
-  #end
+  private
 
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
